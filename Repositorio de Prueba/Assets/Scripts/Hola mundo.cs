a6b835265f69f8506ebb47849e6560e7802b7b22
Urol45 <joanurdanet60@gmail.com>
2026-05-27 00:07:37 -0400

    Hola mundo
    Creacion de "Hola mundo" y se agrega otro layout al editor de Unity

---
+++ Repositorio de Prueba/Assets/Scripts/Hola mundo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Holamundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Algo salio bien");
+        Debug.LogWarning("Algo Salio rarete");
+        Debug.LogError("Algo salio muy pero que muy mal");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
