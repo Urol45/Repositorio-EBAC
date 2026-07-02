cbbdf4ca5543714e517f72145b1a5ed6ebe2961c
Urol45 <joanurdanet60@gmail.com>
2026-07-02 18:15:51 -0400

    Componentes 1, 2 y 3
    Se agregan componentes 1, 2 y 3 y se actualiza script de "Hola mundo" con las funciones aprendidas

--- ProyectoUnity/Assets/Scripts/Hola mundo.cs
+++ ProyectoUnity/Assets/Scripts/Hola mundo.cs
@@ -1,19 +1,46 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Holamundo : MonoBehaviour
 {
+    int x;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Algo paso");
-        Debug.Log("Algo paso");
-        Debug.LogWarning("Algo salio medianamente mal");
-        Debug.LogError("Algo salio muy mal");
+        x = 0;
+        print("algo paso");
+        Debug.LogWarning("algo salio medianamente mal");
+        Debug.LogError("algo salio muy mal");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("Hola desde Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto ha sido deshabilitado");
     }
 }
